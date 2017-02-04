1c46d118d975f89249d3e012fec1bd5035800de7
郭虓 <guoxiao@guoxiaodeMacBook-Air.local>
2017-02-04 10:18:10 +0800

    init for laravel learning

---
+++ Laravel/resources/views/layouts/default.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>sample app</title>
+      <link rel='stylesheet' href="/css/app.css">
+    </head>
+    <body>
+        <header class="navbar navbar-fixed-top navbar-inverse">
+            <div class="container">
+                <div class="col-md-offset-1 col-md-10">
+                   <a href="/" id="logo"> sample app</a>
+                   <nav>
+                       <ul class="nav navbar-nav navbar-right">
+                           <li><a href="/help">help</a></li>
+                           <li><a href="/about">about</a></li>
+                       </ul>
+                   </nav>
+                </div>
+            </div>
+        </header>
+        <div class="container">
+        @yield('content')
+        </div>
+    </body>
+</html>
---
+++ Laravel/resources/views/staticPages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('content')      
+<div class="jumbotron">
+<h1>home</h1>
+</div>
+@stop
